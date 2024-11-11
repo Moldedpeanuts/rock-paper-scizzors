9ed3338d33ca25ef627f0ebc12320f26aadef17b
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-11-11 12:03:20 +0200

    Feat: store the random number generated by Math floor into a variable

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ console.log("Welcome to Rock-Paper-Scissors in the console!")
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
+
+// Computer's choice
+
+let randomChoice = Math.floor(Math.random()*3);
+
